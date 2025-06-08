bbb0df989e6fd8001e65c6a3d2e9d870651b8015
Артур Саяхов <artursaahov@MacBook-Air.local>
2025-06-08 21:47:01 +0500

    fixed

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 
 import './Header.css'
+import { ReactComponent as HeaderSvg } from "../images/header_img.svg";
 import mainImg from "../images/header_img.svg"
 // import lineImg from "../images/line.svg"
 
@@ -8,7 +9,7 @@ const Header = () => {
   return (
     <>
       <div className="main__container">
-        <img className="main_img" src={mainImg} alt="main_img" loading="lazy" decoding="async"/>
+        <HeaderSvg className="main_img" />
         {/* <img className="line_img" src={lineImg} alt="line_img" /> */}
         <p className='header__text'>Artur&Elza</p>
       </div>
